ef8e8ee8ff61a278daa3b54863ecb70c000f7444
Blotjarl <marshall.junior03@gmail.com>
2025-07-31 01:06:35 -0500

    Initialized backend with Express + Socket.io

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Overthrow server is running!");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
